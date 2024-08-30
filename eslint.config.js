8b382bbdf20e56083e39a4342037e2e738812478
Bellangelo <aggelosbellos7@gmail.com>
2024-08-30 18:32:53 +0300

    Initialised javascript project

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**", "tests/**"],
+  }
+]
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+    testEnvironment: 'jest-environment-node',
+    clearMocks: true,
+    moduleFileExtensions: ['js'],
+    testMatch: ['**/*.test.js'],
+    verbose: true,
+    "transform": {},
+}
