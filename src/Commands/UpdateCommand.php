ecaf20d7ac6b551232fd227fd20ed5ad935c1e4e
Bellangelo <aggelosbellos7@gmail.com>
2024-09-02 05:03:03 +0300

    Skeleton of the update command

--- src/Commands/UpdateCommand.php
+++ src/Commands/UpdateCommand.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Bellangelo\TypeCoverageAutoUpdate\Commands;
 
+use Bellangelo\TypeCoverageAutoUpdate\Factories\AnalyseProcessFactory;
+use Bellangelo\TypeCoverageAutoUpdate\Factories\PHPStanConfigFactory;
+use Bellangelo\TypeCoverageAutoUpdate\PHPStanOutput;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Update extends Command
+class UpdateCommand extends Command
 {
     protected function configure(): void
     {
@@ -18,6 +21,15 @@ class Update extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $output->writeln('Finding configuration...');
+
+        $config = (new PHPStanConfigFactory())->create();
+        $process = (new AnalyseProcessFactory())->create(__DIR__ . '/../../phpstan.neon');
+
+        $process->run();
+
+        $output = new PHPStanOutput($config, $process->getOutput());
+
         return Command::SUCCESS;
     }
 }
