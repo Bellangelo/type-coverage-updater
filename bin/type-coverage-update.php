13b558f815eab192fa98e0ef6d13ba2b94ecf874
Bellangelo <aggelosbellos7@gmail.com>
2024-09-02 03:58:55 +0300

    Fix typo

--- bin/type-coverage-update.php
+++ bin/type-coverage-update.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-use Bellangelo\TypeCoverageAutoUpdate\Commands\Update;
+use Bellangelo\TypeCoverageAutoUpdate\Commands\UpdateCommand;
 use Symfony\Component\Console\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $application = new Application();
-$application->add(new Update());
+$application->add(new UpdateCommand());
 $application->run();
