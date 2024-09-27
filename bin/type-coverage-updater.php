5fe9127d80370debc8e00376f310e392ff753787
Bellangelo <aggelosbellos7@gmail.com>
2024-09-27 04:55:40 +0300

    Use the global variable

--- bin/type-coverage-updater.php
+++ bin/type-coverage-updater.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Bellangelo\TypeCoverageUpdater\Commands\UpdateCommand;
 use Symfony\Component\Console\Application;
 
+require_once $_composer_autoload_path ?? __DIR__ . '/../vendor/autoload.php';
+
 $application = new Application();
 $application->add(new UpdateCommand());
 $application->run();
