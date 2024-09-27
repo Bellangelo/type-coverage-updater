367ff694901923ec2226033e45db98adef0d5c17
Bellangelo <aggelosbellos7@gmail.com>
2024-09-27 04:51:14 +0300

    Since file is copied to another place the path won't work

--- bin/type-coverage-updater.php
+++ bin/type-coverage-updater.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 use Bellangelo\TypeCoverageUpdater\Commands\UpdateCommand;
 use Symfony\Component\Console\Application;
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 $application = new Application();
 $application->add(new UpdateCommand());
 $application->run();
